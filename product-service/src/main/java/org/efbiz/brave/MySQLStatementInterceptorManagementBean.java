72a473819e5b4dd76f227a36683f0709b6247300
订单组-谭智军 <thanos_t@163.com>
2017-04-28 10:24:13 +0800

    增加MySQL日志调用跟踪

---
+++ product-service/src/main/java/org/efbiz/brave/MySQLStatementInterceptorManagementBean.java
@@ -0,0 +1,21 @@
+package org.efbiz.brave;
+
+import com.github.kristofa.brave.ClientTracer;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * A simple bean whose only purpose in life is to manage the lifecycle of the {@linkplain ClientTracer} in the {@linkplain MySQLStatementInterceptor}.
+ */
+public class MySQLStatementInterceptorManagementBean implements Closeable {
+
+    public MySQLStatementInterceptorManagementBean(final ClientTracer tracer) {
+        MySQLStatementInterceptor.setClientTracer(tracer);
+    }
+
+    @Override
+    public void close() throws IOException {
+        MySQLStatementInterceptor.setClientTracer(null);
+    }
+}
